626ecadd0995fdfa676ce99a600f41f1c39a6178
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-17 21:46:41 +0600

    nav bar added & img added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('environment setup completed!')
+//  Loading Catagory Data From the API
+const loadCategory = () =>{
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategoryData(data.data.news_category))
+}
+
+const displayCategoryData = categories => {
+    // console.log(catagories.news_category)
+    // console.log(categories);
+    
+    categories.forEach(element =>{
+        console.log(element.category_name);
+    });
+}
+
+
+loadCategory();
